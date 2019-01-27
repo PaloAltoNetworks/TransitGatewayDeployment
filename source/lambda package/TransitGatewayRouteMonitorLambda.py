eab998113a06049196a77b6abae1a159ec0102a5
jharris10 <jharris@paloaltonetworks.com>
2019-01-27 17:28:30 +0000

    Added additional comments and log

--- source/lambda package/TransitGatewayRouteMonitorLambda.py
+++ source/lambda package/TransitGatewayRouteMonitorLambda.py
@@ -17,10 +17,11 @@
 """
 Palo Alto Networks TransitGatewayRouteMonitorLambda.py
 
-Script triggered by a Cloudwatch event that will monitor the health of the firewalls
+Script triggered by a Cloudwatch event that will monitor the health of firewalls
 via the "show chassis status" op command on the Trust interface.
-The purpose is to assess the health of the firewall and modifiy the route AWS route table to redirect traffic if the
-firewall is down.
+The purpose is to assess the health of the firewall and modify an AWS route table to redirect
+traffic if the firewall is down.  When I firewall goes down routes within the route table bound to the
+TGW attachment will show next hop as blackhole.  The routes need to be updated to a functional eni.
 
 This software is provided without support, warranty, or guarantee.
 Use at your own risk.
@@ -50,14 +51,17 @@ events_client = boto3.client('events')
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
+
 def check_for_split_routes(route_table_id, vpc_summary_route, def_route):
     """
-    Checks the route table if split routes is True and both VPC Summary and Default point to the same eni
-    Return False else Return True
-    :param route_table_id:
-    :param vpc_summary_route:
-    :param def_route:
+    Checks the route table if split_routes == True and if both the vpc_summary and Default point to the same eni
+    Return False else Return True.  When split routes is True we want to use both firewalls.  Firewall 1 for internet
+    traffic and firewall 2 for east/west traffic.
 
+    :param route_table_id:  The route table that we will modify.
+    :param vpc_summary_route:  A summary route used to forward all east west traffic to the alternative firewall if
+    required
+    :param def_route: The default route in this case 0.0.0.0/0
     :param route_table_id: route table the we need to check
     :return: True/False
 
@@ -76,15 +80,17 @@ def check_for_split_routes(route_table_id, vpc_summary_route, def_route):
             elif route['DestinationCidrBlock'] == def_route:
                 def_route_eni = eni1
     if vpc_summary_route_eni == def_route_eni:
+        logger.info("Both routes use eni {0}".format(def_route_eni))
         return False
     else:
         return True
 
+
 def replace_vpc_route_to_fw(route_table_id, destination_cidr_block, NetworkInterfaceId, DryRun=False):
     """
-    Scan the route table for blackhole routes where the next hop is the NIC of the failed firewall
-    Delete any routes that are either Default routes or a summary route of all the spoke VPC's
-    Add new routes for hte default and vpc summary route where the next hop is NetworkIntefaceId
+    Scan the route table for blackhole routes where the next hop is the eni of the failed firewall.
+    In order to replace the routes we first delete the route and then add a new route pointing to the
+    backup eni.
 
     :param route_table_id: The route table that requires modification
     :param destination_cidr_block: The cidr block that we need to change.  Normally the default route and VPC summary route
@@ -120,20 +126,14 @@ def replace_vpc_route_to_fw(route_table_id, destination_cidr_block, NetworkInter
 
 def failover(route_table_id, failed_eni, backup_eni):
     """
-
-    :param route_table_id:
-    :param failed_eni:
-    :param backup_eni:
-    :return:
     Looks for routes that are blackholed by the failure of the firewall
-    When it finds a route it call replance_vpc_route_to_fw to update the next hop to a functional eni
+    When it finds a route it will call replace_vpc_route_to_fw to update the next hop to a functional eni
 
     :param route_table_id: The route table that requires modification
     :param failed_eni: NetworkInterfaceId: The eni of the Firewall that has failed
     :param backup_eni: NetworkInterfaceId: The eni of the Firewall that we need to failover to
     :return:
     """
-
 
     route_table = ec2_client.describe_route_tables(RouteTableIds=[route_table_id])
     Interfaceids = []
@@ -144,7 +144,7 @@ def failover(route_table_id, failed_eni, backup_eni):
             eni1 = route['NetworkInterfaceId']
             if route['NetworkInterfaceId'] == failed_eni:
                 logger.info("Found route {} with blackhole next hop {}".format(failed_eni,
-                            route['DestinationCidrBlock']))
+                                                                               route['DestinationCidrBlock']))
                 destination_cidr_block = route['DestinationCidrBlock']
                 replace_vpc_route_to_fw(route_table_id, destination_cidr_block, backup_eni, DryRun=False)
 
@@ -199,6 +199,28 @@ def get_firewall_status(gwMgmtIp, api_key):
 
 
 def lambda_handler(event, context):
+    '''
+    Controls the failover of routing of traffic between VPC's and to the internet.   In the event of a failure the
+    backup firewall will provide routing and security
+
+
+    preempt = os.environ['preempt'] Set this value to TRUE if you wish the firewalls to return to an Active/Active state
+    as soon as the failed firewall becomees healthy again or set it to true in the environment variables during a change
+    window.
+    vpc_summary_route = os.environ['VpcSummaryRoute'] Set thus value as a route that summarises wth VPC spokes. The
+    security VPC should not be contained in this summary route.
+    fw1_trust_eni = os.environ['fw1Trusteni']  Fw 1 trust eni id
+    fw2_trust_eni = os.environ['fw2Trusteni']  Fw 1 trust eni id
+    route_table_id = os.environ['fromTGWRouteTableId']  Route table id of the route table associated with the TGW attachment
+    fw1_trust_ip = os.environ['fw1Trustip'] FW Trust Inteface IP used for health probies.
+    fw2_trust_ip = os.environ['fw2Trustip'] FW Trust Inteface IP used for health probies.
+    api_key = os.environ['apikey']
+    split_routes = os.environ['splitroutes'] Select True if you intend to use both firewalls One for east/West and
+    one for internet.
+    :param event:
+    :param context:
+    :return:
+    '''
 
     preempt = os.environ['preempt']
     vpc_summary_route = os.environ['VpcSummaryRoute']
@@ -211,11 +233,6 @@ def lambda_handler(event, context):
     split_routes = os.environ['splitroutes']
 
     def_route = '0.0.0.0/0'
-
-    check_for_split_routes(route_table_id, vpc_summary_route, def_route)
-
-    # fw1_trust_eni = 'eni-09539d453383a172a'
-    # fw2_trust_eni = 'eni-04fe20fe0f4f3f374'
 
     global gcontext
 
