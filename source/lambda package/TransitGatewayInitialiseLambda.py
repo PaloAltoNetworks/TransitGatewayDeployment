7b930aec578f08895f592d529522e264e652de3a
jharris10 <jharris@paloaltonetworks.com>
2019-01-30 23:34:17 +0200

    "Documentation edits"

--- source/lambda package/TransitGatewayInitialiseLambda.py
+++ source/lambda package/TransitGatewayInitialiseLambda.py
@@ -1,9 +1,17 @@
-# from commonLambdaFunctions import fetchFromTransitConfigTable, publishToSns, sendToQueue
+"""
+Palo Alto Networks TransitGatewayInitialiseLambda.py
 
+Script triggered by a Cloudformation that will create route table entries in each VPC that use next hop of the
+transit gateway attachment.  We use this script today as next hop TransitGatewayId is not supported in CFT yet
+When CFT use the updated boto3 libraries we can remove this function and place the route entries using CFT.
+
+This software is provided without support, warranty, or guarantee.
+Use at your own risk.
+"""
 import logging
 import os
-
 import boto3
+import cfnresponse
 
 
 logger = logging.getLogger()
@@ -14,6 +22,13 @@ vnetroutecidr = '10.0.0.0/8'
 
 
 def add_route_tgw_nh(route_table_id, destination_cidr_block, transit_gateway_id):
+    '''
+    Adds a route to a VPC route table with next hop of the TransitGatewayId
+    :param route_table_id:
+    :param destination_cidr_block:
+    :param transit_gateway_id:
+    :return:
+    '''
     ec2 = boto3.client('ec2')
 
     resp = ec2.create_route(
@@ -25,15 +40,14 @@ def add_route_tgw_nh(route_table_id, destination_cidr_block, transit_gateway_id)
     logger.info("Got response to add_route_tgw_nh {} ".format(resp))
     return resp
 
-def add_route_fw(fw_ip, tgi_attach_eni, prefix):
-    logger.info ("Adding routes to firewall route table")
-    
-def get_tgw_attchment_eni(vpc_id):
-    logger.info ("Getting the TGW attachment ENI for each trust subnet")
-
 def delete_route(route_table_id, destination_cidr_block):
+    '''
+    Deletes a route from the VPC route table
+    :param route_table_id:
+    :param destination_cidr_block:
+    :return:
+    '''
     ec2 = boto3.client('ec2')
-
     resp = ec2.delete_route(
         DestinationCidrBlock=destination_cidr_block,
         RouteTableId=route_table_id,
@@ -43,10 +57,15 @@ def delete_route(route_table_id, destination_cidr_block):
 
 
 def lambda_handler(event, context):
+    '''
+    Each VPC (including the security VPC) requires a static route directing traffic with a next hop of the
+    TransitGatewayId.   In this case we take to route table id and  TransitGatewayId via environment variables from
+    the CFT template.
+    :param event:
+    :param context:
+    :return:
+    '''
     logger.info("Got event {} ".format(event))
-    fw1InstanceId = os.environ['fw1InstanceId']
-    fw2InstanceId = os.environ['fw2InstanceId']
-
     region = os.environ['region']
     toTGWRouteTable = os.environ['toTGWRouteTableId']
     VPC0_route_table_id = os.environ['vpc0HostRouteTableid']
@@ -71,7 +90,7 @@ def lambda_handler(event, context):
     elif event['RequestType'] == 'Delete':
         print("Got Delete event")
         try:
-            res = delete_route(fromTGWRouteTable, vnetroutecidr)
+            res = delete_route(toTGWRouteTable, vnetroutecidr)
             res1 = delete_route(VPC0_route_table_id, defroutecidr)
             result = cfnresponse.send(event, context, cfnresponse.SUCCESS, responseData, "CustomResourcePhysicalID")
 
